54a5d4402dd9ef7ecba4cf40c28caa6b0543ef59
Jonas Valereo <jvalereo2016@gmail.com>
2019-03-28 14:39:03 -0300

    Testando Array Multidimensional
    Projeto, Programa Teste em C#, Array Multidimensional

---
+++ AulaArrayMulti/AulaArrayMulti/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AulaArrayMulti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblTitulo2 = new System.Windows.Forms.Label();
+            this.BtnTeste = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(67, 19);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(152, 22);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Testando Array";
+            // 
+            // lblTitulo2
+            // 
+            this.lblTitulo2.AutoSize = true;
+            this.lblTitulo2.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo2.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo2.Location = new System.Drawing.Point(77, 51);
+            this.lblTitulo2.Name = "lblTitulo2";
+            this.lblTitulo2.Size = new System.Drawing.Size(129, 19);
+            this.lblTitulo2.TabIndex = 1;
+            this.lblTitulo2.Text = "Multidimesional";
+            // 
+            // BtnTeste
+            // 
+            this.BtnTeste.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTeste.Location = new System.Drawing.Point(51, 121);
+            this.BtnTeste.Name = "BtnTeste";
+            this.BtnTeste.Size = new System.Drawing.Size(178, 32);
+            this.BtnTeste.TabIndex = 2;
+            this.BtnTeste.Text = "Array Multidimensional";
+            this.BtnTeste.UseVisualStyleBackColor = true;
+            this.BtnTeste.Click += new System.EventHandler(this.BtnTeste_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(103, 195);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(81, 30);
+            this.BtnSair.TabIndex = 3;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnTeste);
+            this.Controls.Add(this.lblTitulo2);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste Array Multidimensional";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblTitulo2;
+        private System.Windows.Forms.Button BtnTeste;
+        private System.Windows.Forms.Button BtnSair;
+    }
+}
+
---
+++ AulaArrayMulti/AulaArrayMulti/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaArrayMulti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTeste_Click(object sender, EventArgs e)
+        {
+            //Aula Teste Array Multidimensional 
+            //Jonas Valereo Técnico em Informática 
+
+            //Declarando o codigo Array e instanciando vetor nota e qte de linhas e colunas
+
+
+            double[,] notas = new double[2, 4];
+
+            notas[0, 0] = 8.0;
+            notas[0, 1] = 7.5;
+            notas[0, 2] = 9.0;
+            notas[0, 3] = 8.5;
+            notas[1, 0] = 9.5;
+            notas[1, 1] = 8.5;
+            notas[1, 2] = 8.0;
+            notas[1, 3] = 9.0;
+
+            //Declarando o MessageBox a exibido as devidas notas escolhidas
+
+            MessageBox.Show(notas[0,0].ToString(), "Array");
+            MessageBox.Show(notas[0,1].ToString(),"Array");
+            MessageBox.Show(notas[0,2].ToString(), "Array");
+            MessageBox.Show(notas[0,3].ToString(), "Array");
+            MessageBox.Show(notas[1,0].ToString(), "Array");
+            MessageBox.Show(notas[1,1].ToString(), "Array");
+            MessageBox.Show(notas[1,2].ToString(), "Array");
+            MessageBox.Show(notas[1,3].ToString(), "Array");
+      
+
+            //Fim do programa 
+
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            //Declarando o codigo close, sair
+
+            Close();
+
+        }
+    }
+}
